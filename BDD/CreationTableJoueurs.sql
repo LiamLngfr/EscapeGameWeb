69a20cd1931a6a4a3fcd99ac9f52e40dc6b088e3
gpotel <gabinpot@gmail.com>
2024-11-21 00:34:13 +0100

    Ajout fichier de création de la table Joueurs

---
+++ BDD/CreationTableJoueurs.sql
@@ -0,0 +1,16 @@
+-- Table: public.joueurs
+
+DROP TABLE IF EXISTS public.joueurs;
+
+CREATE TABLE IF NOT EXISTS public.joueurs
+(
+    id SERIAL PRIMARY KEY,
+    pseudo VARCHAR(255) NOT NULL
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.joueurs
+    OWNER to postgres;
+
+DELETE FROM joueurs; --J'ai copié ton truc pour items, mais pas sûr que cette ligne soit vraiment utile
